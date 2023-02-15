f496b72dd15f5d4df3f709d025d2ce06e91c809e
JoJozsef <boli.jozsef@gmail.com>
2023-02-15 20:55:46 +0100

    Add project files.

---
+++ ProbGame/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace ProbGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblPupol = new System.Windows.Forms.Label();
+            this.btnFood = new System.Windows.Forms.Button();
+            this.LblMaxPopu = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Food = new System.Windows.Forms.Label();
+            this.lblFood = new System.Windows.Forms.Label();
+            this.lblMaxFood = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblWood = new System.Windows.Forms.Label();
+            this.lblMaxWood = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblRock = new System.Windows.Forms.Label();
+            this.lblMaxRock = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnWood = new System.Windows.Forms.Button();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblMaxRock);
+            this.groupBox1.Controls.Add(this.lblMaxWood);
+            this.groupBox1.Controls.Add(this.lblMaxFood);
+            this.groupBox1.Controls.Add(this.LblMaxPopu);
+            this.groupBox1.Controls.Add(this.lblRock);
+            this.groupBox1.Controls.Add(this.lblWood);
+            this.groupBox1.Controls.Add(this.lblFood);
+            this.groupBox1.Controls.Add(this.LblPupol);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Food);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(226, 499);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Matherials";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Population";
+            // 
+            // LblPupol
+            // 
+            this.LblPupol.AutoSize = true;
+            this.LblPupol.Location = new System.Drawing.Point(109, 43);
+            this.LblPupol.Name = "LblPupol";
+            this.LblPupol.Size = new System.Drawing.Size(13, 13);
+            this.LblPupol.TabIndex = 0;
+            this.LblPupol.Text = "0";
+            // 
+            // btnFood
+            // 
+            this.btnFood.Location = new System.Drawing.Point(320, 86);
+            this.btnFood.Name = "btnFood";
+            this.btnFood.Size = new System.Drawing.Size(75, 23);
+            this.btnFood.TabIndex = 1;
+            this.btnFood.Text = "Food";
+            this.btnFood.UseVisualStyleBackColor = true;
+            this.btnFood.Click += new System.EventHandler(this.btnFood_Click);
+            // 
+            // LblMaxPopu
+            // 
+            this.LblMaxPopu.AutoSize = true;
+            this.LblMaxPopu.Location = new System.Drawing.Point(166, 43);
+            this.LblMaxPopu.Name = "LblMaxPopu";
+            this.LblMaxPopu.Size = new System.Drawing.Size(13, 13);
+            this.LblMaxPopu.TabIndex = 0;
+            this.LblMaxPopu.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "/";
+            // 
+            // Food
+            // 
+            this.Food.AutoSize = true;
+            this.Food.Location = new System.Drawing.Point(7, 79);
+            this.Food.Name = "Food";
+            this.Food.Size = new System.Drawing.Size(31, 13);
+            this.Food.TabIndex = 0;
+            this.Food.Text = "Food";
+            // 
+            // lblFood
+            // 
+            this.lblFood.AutoSize = true;
+            this.lblFood.Location = new System.Drawing.Point(109, 79);
+            this.lblFood.Name = "lblFood";
+            this.lblFood.Size = new System.Drawing.Size(13, 13);
+            this.lblFood.TabIndex = 0;
+            this.lblFood.Text = "0";
+            // 
+            // lblMaxFood
+            // 
+            this.lblMaxFood.AutoSize = true;
+            this.lblMaxFood.Location = new System.Drawing.Point(166, 79);
+            this.lblMaxFood.Name = "lblMaxFood";
+            this.lblMaxFood.Size = new System.Drawing.Size(13, 13);
+            this.lblMaxFood.TabIndex = 0;
+            this.lblMaxFood.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(138, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Wood";
+            // 
+            // lblWood
+            // 
+            this.lblWood.AutoSize = true;
+            this.lblWood.Location = new System.Drawing.Point(109, 113);
+            this.lblWood.Name = "lblWood";
+            this.lblWood.Size = new System.Drawing.Size(13, 13);
+            this.lblWood.TabIndex = 0;
+            this.lblWood.Text = "0";
+            // 
+            // lblMaxWood
+            // 
+            this.lblMaxWood.AutoSize = true;
+            this.lblMaxWood.Location = new System.Drawing.Point(166, 113);
+            this.lblMaxWood.Name = "lblMaxWood";
+            this.lblMaxWood.Size = new System.Drawing.Size(13, 13);
+            this.lblMaxWood.TabIndex = 0;
+            this.lblMaxWood.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(138, 113);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(12, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "/";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 145);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Rock";
+            // 
+            // lblRock
+            // 
+            this.lblRock.AutoSize = true;
+            this.lblRock.Location = new System.Drawing.Point(108, 145);
+            this.lblRock.Name = "lblRock";
+            this.lblRock.Size = new System.Drawing.Size(13, 13);
+            this.lblRock.TabIndex = 0;
+            this.lblRock.Text = "0";
+            // 
+            // lblMaxRock
+            // 
+            this.lblMaxRock.AutoSize = true;
+            this.lblMaxRock.Location = new System.Drawing.Point(165, 145);
+            this.lblMaxRock.Name = "lblMaxRock";
+            this.lblMaxRock.Size = new System.Drawing.Size(13, 13);
+            this.lblMaxRock.TabIndex = 0;
+            this.lblMaxRock.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(137, 145);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(12, 13);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "/";
+            // 
+            // btnWood
+            // 
+            this.btnWood.Location = new System.Drawing.Point(414, 87);
+            this.btnWood.Name = "btnWood";
+            this.btnWood.Size = new System.Drawing.Size(75, 23);
+            this.btnWood.TabIndex = 1;
+            this.btnWood.Text = "Wood";
+            this.btnWood.UseVisualStyleBackColor = true;
+            this.btnWood.Click += new System.EventHandler(this.btnFood_Click);
+            // 
+            // btnRock
+            // 
+            this.btnRock.Location = new System.Drawing.Point(523, 87);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(75, 23);
+            this.btnRock.TabIndex = 1;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnFood_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1115, 690);
+            this.Controls.Add(this.btnRock);
+            this.Controls.Add(this.btnWood);
+            this.Controls.Add(this.btnFood);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMaxFood;
+        private System.Windows.Forms.Label LblMaxPopu;
+        private System.Windows.Forms.Label lblFood;
+        private System.Windows.Forms.Label LblPupol;
+        private System.Windows.Forms.Label Food;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFood;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblMaxRock;
+        private System.Windows.Forms.Label lblMaxWood;
+        private System.Windows.Forms.Label lblRock;
+        private System.Windows.Forms.Label lblWood;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnWood;
+        private System.Windows.Forms.Button btnRock;
+    }
+}
+
---
+++ ProbGame/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProbGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblMaxFood.Text = "20";
+        }
+
+        private void btnFood_Click(object sender, EventArgs e)
+        {
+            int temp = 2;
+            if (lblFood.Text == "0")
+            {
+                lblFood.Text = "" + 2;
+            } else
+            {
+                lblFood.Text = (Convert.ToInt32(lblFood.Text) + 2).ToString();
+            }
+            
+        }
+    }
+}
